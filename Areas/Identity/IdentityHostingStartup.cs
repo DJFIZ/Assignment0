bef5d20f120f54634623882c41202db5f74c7871
djfiz <djfiz@sbcglobal.net>
2021-08-06 16:15:37 -0700

    Updated packages/removed deprecated references

--- Areas/Identity/IdentityHostingStartup.cs
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -2,7 +2,6 @@
 using Assignment0.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
