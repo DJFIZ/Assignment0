327169612e953465db6a21c4d3636ecd0ec93e9a
djfiz <djfiz@sbcglobal.net>
2021-11-18 11:16:57 -0800

    Added html encoding to prevent xss

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -4,7 +4,9 @@ using Assignment0.VIewModels;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Security.Application;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Assignment0.Controllers
 {
@@ -12,6 +14,7 @@ namespace Assignment0.Controllers
     {
         private readonly AppDbContext _appDbContext;
         private readonly IMediator _mediator;
+        //private readonly AntiXssEncoder _antiXssEncoder;
 
         public BlogController(AppDbContext appDbContext, IMediator mediator)
         {
@@ -19,8 +22,6 @@ namespace Assignment0.Controllers
             _mediator = mediator;
         }
 
-        //  IActionResult is a base interface that I tend to use as the return type of Actions in case
-        //  I have an if statement that returns different results (i.e., ViewResult vs. ContentResult).
         public async Task<IActionResult> List()
         {
             BlogsListViewModel blogsListViewModel = new(); // This is new C# 9 syntax; not required, but I like it.
@@ -31,28 +32,6 @@ namespace Assignment0.Controllers
             return View(blogsListViewModel);
         }
 
-        /*
-        When accessing any resource that requires I/O, prefer the async version of method calls.
-
-        To access child comments for a blog post, .Include() the navigation property.
-
-        Since BlogId is the primary key on the Blogs database table, use SingleOrDefaultAsync(); there should
-        be at most one Blog record with a given id.
-
-        Also, since this action only reads entities from the database, call AsNoTracking() to avoid the
-        overhead of tracking the entities in EF Core. You only need entity tracking if you're going to 
-        load an entity, change it, and then save it back to the database in a single web request.
-        */
-        /*
-        2021-10-21: I updated this method to use MediatR and view models. Since the data model is so simple,
-        the benefits aren't immediately apparent, but this is the method we use to ensure the ultimate flexibility
-        in shaping the data for display.
-
-        If this action posted data back to the server, you would see the separation of validation. We'd put the
-        validation on the view models instead of relying on the validation on the EF entities. UI validation can
-        subtly differ from database validation - for example, a field might be non-nullable in the database but
-        nullable in the UI.
-        */
         public async Task<IActionResult> Details(BlogDetails.Query query)
         {
             // This executes the code in the BlogDetails.QueryHandler class and returns the result.
@@ -87,7 +66,7 @@ namespace Assignment0.Controllers
                 .Include(b => b.Comments)
                 .SingleOrDefaultAsync(b => b.BlogId == id);
 
-            var c = new Comment { BlogId = id, Author = name, Body = comment, Date = System.DateTime.Now };
+            var c = new Comment { BlogId = id, Author = HttpUtility.HtmlEncode(name), Body = HttpUtility.HtmlEncode(comment), Date = System.DateTime.Now };
 
             blog.NumComments++;
             await _appDbContext.Comments.AddAsync(c);
